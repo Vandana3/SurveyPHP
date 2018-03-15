4b1ae708ad8fad8f48d23b3c645ce250bbb65951
Vandana <vandana121197@gmail.com>
2018-03-15 21:13:36 +0530

    SurveySite

---
+++ survey/index.php
@@ -0,0 +1,52 @@
+<html>
+    <head>
+       <style>
+div.container {
+    width: 100%;
+    border: 1px solid gray;
+	font-size: 110%;
+	
+}
+
+header{
+	
+    padding: 1em;
+    color: white;
+    background-color: black;
+    clear: left;
+    text-align: center;
+	
+}
+article {
+	height:100%;
+    margin-left: 20px;
+    border-left: 1px solid gray;
+    padding: 1em;
+    overflow: hidden;
+}
+footer {
+	padding: 1em;
+    color: white;
+    background-color: black;
+    clear: left;
+    text-align: center;
+    bottom: 0;
+	position: fixed;
+	width:98%;
+	}
+</style>
+    </head>
+    <body>
+        <div class="container">
+		<header><h1> WELCOME TO SURVEY SITE </h1> </header>
+		<article>
+        <h2 style="text-align: center"> <a href="register.php"> CLICK HERE TO REGISTER </a> </h2>
+		<br><br>
+		<h2 style="text-align: center"> <a href="login.php"> LOGIN </a> </h2>
+		</article>
+	<footer>
+	<h2> Thank you for visiting </h2>
+	</footer>
+	</div>
+	</body>
+</html> 
